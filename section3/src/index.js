7a0b5af3067a22c8dd181fb52e4f8dac9e05a1f8
hwang dan bee <eksql5557@naver.com>
2024-07-21 09:30:22 +0900

    practice01
    practice01

---
+++ section3/src/index.js
@@ -0,0 +1,14 @@
+// const { add, sub } = require("./math");
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+
+// import { add, sub } from"./math.js";
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+
+ 
